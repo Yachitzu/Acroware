08d8ac71339ddd6f7a2f8702a7475b49a4af60e3
Cifu2002 <estebancifu88@gmail.com>
2024-05-11 15:47:55 -0500

    Se agrego la conexión a la base de datos

---
+++ patrones/Singleton/Conexion.php
@@ -0,0 +1,34 @@
+<?php
+    class Conexion{
+
+        private static $instance;
+        private $conexion;
+
+        private function __construct(){
+            $host = "localhost";
+            $user = "root";
+            $password = "";
+            $database = "das";
+
+            try {
+                $dsn = "mysql:host=$host; dbname=$database;charset=utf8mb4"; // Añadir codificación
+                $this->conexion = new PDO($dsn, $user, $password);
+                $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                // En producción, considera registrar este error en lugar de mostrarlo
+                die("Error al conectar a la base de datos: " . $e->getMessage());
+            }
+        }
+
+        public static function getInstance(){
+            if(!self::$instance){
+                self::$instance = new Conexion();
+            }
+            return self::$instance;
+        }
+
+        public function getConexion(){
+            return $this -> conexion;
+        }
+    }
+?>
