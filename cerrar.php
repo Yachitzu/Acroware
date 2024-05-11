71db34b694aa50c89faa5cbdc8a76ebdac12d178
Cifu2002 <estebancifu88@gmail.com>
2024-05-11 16:51:27 -0500

    Se creo el backend de cierre de sesion

---
+++ cerrar.php
@@ -0,0 +1,5 @@
+<?php
+    include_once("C:/xampp/htdocs/SOA/sistemas/DAS/Acroware/patrones/Strategy/Authenticator.php");
+    $authenticator = new Authenticator();
+    $authenticator-> closeSession();
+?>
