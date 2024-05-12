576e91f4ae4f5200a0862ed5d173d6b46db7dc61
Cifu2002 <estebancifu88@gmail.com>
2024-05-12 01:41:00 -0500

    Se añadio actualizarAreas al Rest

--- Acciones/Rest.php
+++ Acciones/Rest.php
@@ -115,7 +115,7 @@ switch ($accion) {
                 $descripcion = filter_var($data['descripcion'], FILTER_SANITIZE_STRING);
                 $piso = filter_var($data['piso'], FILTER_SANITIZE_STRING);
                 $id_bloque_per = filter_var($data['id_bloque_per'], FILTER_SANITIZE_STRING);
-                $id_usu_encargado =  filter_var($data['id_usu_encargado'], FILTER_SANITIZE_STRING);
+                $id_usu_encargado = filter_var($data['id_usu_encargado'], FILTER_SANITIZE_STRING);
                 $resultado = AccionesAreas::insertarAreas($nombre, $descripcion, $piso, $id_bloque_per, $id_usu_encargado);
                 if ($resultado === 0) {
                     http_response_code(200);
@@ -125,8 +125,25 @@ switch ($accion) {
                     echo json_encode(["message" => "No se pudo insertar la área."]);
                 }
                 break;
+            case 8:
+                $json_input = file_get_contents('php://input');
+                $data = json_decode($json_input, true);
+                $id = filter_var($data['id'], FILTER_SANITIZE_STRING);
+                $nombre = filter_var($data['nombre'], FILTER_SANITIZE_STRING);
+                $descripcion = filter_var($data['descripcion'], FILTER_SANITIZE_STRING);
+                $piso = filter_var($data['piso'], FILTER_SANITIZE_STRING);
+                $id_bloque_per = filter_var($data['id_bloque_per'], FILTER_SANITIZE_STRING);
+                $id_usu_encargado = filter_var($data['id_usu_encargado'], FILTER_SANITIZE_STRING);
+                $resultado = AccionesAreas::actualizarArea($id, $nombre, $descripcion, $piso, $id_bloque_per, $id_usu_encargado);
+                if ($resultado === 0) {
+                    http_response_code(200);
+                    echo json_encode(["message" => "Área actualizada con éxito."]);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(["message" => "No se pudo actualizar el área."]);
+                }
+                break;
 
-                
 
             default:
                 break;
