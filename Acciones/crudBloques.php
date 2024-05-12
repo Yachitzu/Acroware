5cb6eb6b14d71b2f6dd0336470ad3ebb2a541ddf
Cifu2002 <estebancifu88@gmail.com>
2024-05-11 23:57:06 -0500

    Se creo el backend para listar las Facultades existentes

--- Acciones/crudBloques.php
+++ Acciones/crudBloques.php
@@ -48,5 +48,34 @@ class AccionesBloques
             ];
         }
     }
+
+    public static function listarFacultadesInsertar()
+    {
+        try {
+            $conexion = Conexion::getInstance()->getConexion();
+            $consulta = "SELECT * FROM facultades";
+            $resultado = $conexion->prepare($consulta);
+            $resultado->execute();
+            $dato = $resultado->fetchAll(PDO::FETCH_ASSOC);
+            $dato;
+            $tabla = '';
+
+            foreach ($dato as $respuesta) {
+                $tabla .= '
+                    <option value="' . htmlspecialchars($respuesta['id']) . '">' . htmlspecialchars($respuesta['nombre']) . '</option>
+                ';
+            }
+            return [
+                'codigo' => 0,
+                'dato' => $tabla,
+            ];
+        } catch (PDOException $e) {
+            error_log('Error al listar Facultades: ' . $e->getMessage());
+            return [
+                'codigo' => 1,
+                'mensaje' => 'Error al listar facultades: ' . $e->getMessage()
+            ];
+        }
+    }
 }
 ?>
