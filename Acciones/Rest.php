b024d32e847cfb5b35893f054335b7e34c1d4278
Cifu2002 <estebancifu88@gmail.com>
2024-05-12 00:09:29 -0500

    Se añadio eliminarBloques al Rest

--- Acciones/Rest.php
+++ Acciones/Rest.php
@@ -56,16 +56,16 @@ switch ($accion) {
                 }
                 break;
 
-                //BLOQUES
+            //BLOQUES
             case 4:
                 $json_input = file_get_contents('php://input');
                 $data = json_decode($json_input, true);
                 $nombre = filter_var($data['nombre'], FILTER_SANITIZE_STRING);
-                $descripcion= filter_var($data['descripcion'], FILTER_SANITIZE_STRING);
-                $id_facultad_per =filter_var($data['id_facultad_per'], FILTER_SANITIZE_STRING);
+                $descripcion = filter_var($data['descripcion'], FILTER_SANITIZE_STRING);
+                $id_facultad_per = filter_var($data['id_facultad_per'], FILTER_SANITIZE_STRING);
                 $pisos = filter_var($data['pisos'], FILTER_SANITIZE_STRING);
-                $resultado = AccionesBloques::insertarBloques($nombre, $descripcion, $id_facultad_per,$pisos);
-                if ($resultado===0) {
+                $resultado = AccionesBloques::insertarBloques($nombre, $descripcion, $id_facultad_per, $pisos);
+                if ($resultado === 0) {
                     http_response_code(200);
                     echo json_encode(["message" => "Bloque insertado con éxito."]);
                 } else {
@@ -73,24 +73,36 @@ switch ($accion) {
                     echo json_encode(["message" => "No se pudo insertar el bloque."]);
                 }
                 break;
-                case 5:
-                    $json_input = file_get_contents('php://input');
-                    $data = json_decode($json_input, true);
-                    $id = filter_var($data['id'], FILTER_SANITIZE_STRING);
-                    $nombre = filter_var($data['nombre'], FILTER_SANITIZE_STRING) ;
-                    $descripcion= filter_var($data['descripcion'], FILTER_SANITIZE_STRING);
-                    $id_facultad_per = filter_var($data['id_facultad_per'], FILTER_SANITIZE_STRING);
-                    $pisos = filter_var($data['pisos'], FILTER_SANITIZE_STRING);
-                    $resultado = AccionesBloques::actualizarBloques($id, $nombre, $descripcion, $id_facultad_per, $pisos);
-                    if ($resultado===0) {
-                        http_response_code(200);
-                        echo json_encode(["message" => "Bloque actualizado con éxito."]);
-                    } else {
-                        http_response_code(400);
-                        echo json_encode(["message" => "No se pudo actualizar el bloque."]);
-                    }
-                    break;
-
+            case 5:
+                $json_input = file_get_contents('php://input');
+                $data = json_decode($json_input, true);
+                $id = filter_var($data['id'], FILTER_SANITIZE_STRING);
+                $nombre = filter_var($data['nombre'], FILTER_SANITIZE_STRING);
+                $descripcion = filter_var($data['descripcion'], FILTER_SANITIZE_STRING);
+                $id_facultad_per = filter_var($data['id_facultad_per'], FILTER_SANITIZE_STRING);
+                $pisos = filter_var($data['pisos'], FILTER_SANITIZE_STRING);
+                $resultado = AccionesBloques::actualizarBloques($id, $nombre, $descripcion, $id_facultad_per, $pisos);
+                if ($resultado === 0) {
+                    http_response_code(200);
+                    echo json_encode(["message" => "Bloque actualizado con éxito."]);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(["message" => "No se pudo actualizar el bloque."]);
+                }
+                break;
+            case 6:
+                $json_input = file_get_contents('php://input');
+                $data = json_decode($json_input, true);
+                $id = filter_var($data['id'], FILTER_SANITIZE_STRING);
+                $resultado = AccionesBloques::eliminarBloque($id);
+                if ($resultado === 0) {
+                    http_response_code(200);
+                    echo json_encode(["message" => "Bloque eliminado con éxito."]);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(["message" => "No se pudo eliminar el bloque."]);
+                }
+                break;
             default:
                 break;
         }
