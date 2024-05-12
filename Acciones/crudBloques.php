9905f9bbc7e2ddb623a93e655dcff0d513fbd1b0
Cifu2002 <estebancifu88@gmail.com>
2024-05-11 23:52:33 -0500

    Se creo el backend para listar Bloques

---
+++ Acciones/crudBloques.php
@@ -0,0 +1,52 @@
+<?php
+include_once ($_SERVER['DOCUMENT_ROOT'] . '/Acroware/patrones/Singleton/Conexion.php');
+class AccionesBloques
+{
+    public static function listarBloques()
+    {
+        try {
+            $conexion = Conexion::getInstance()->getConexion();
+            $consulta = "SELECT bloques.*, facultades.nombre AS nombre_facultad 
+            FROM bloques 
+            INNER JOIN facultades ON bloques.id_facultad_per = facultades.id";
+            $resultado = $conexion->prepare($consulta);
+            $resultado->execute();
+            $dato = $resultado->fetchAll(PDO::FETCH_ASSOC);
+            $dato;
+            $tabla = '';
+
+            foreach ($dato as $respuesta) {
+                $tabla .= '
+                    <tr>
+                        <td ">' . htmlspecialchars($respuesta['id']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['nombre']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['descripcion']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['nombre_facultad']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['pisos']) . '</td>
+                        <td class="mdl-data-table__cell">
+                            <center>
+                            <button class="btn btn-warning btn-circle element-white editar" data-id="' . $respuesta['id'] . '" id="editar">
+                            <i class="fas fa-edit"></i>
+                        </button>
+                        <button class="btn btn-danger btn-circle eliminar" id="eliminar">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                            </center>
+                        </td>
+                    </tr>
+                ';
+            }
+            return [
+                'codigo' => 0,
+                'dato' => $tabla,
+            ];
+        } catch (PDOException $e) {
+            error_log('Error al listar bloques: ' . $e->getMessage());
+            return [
+                'codigo' => 1,
+                'mensaje' => 'Error al listar bloques: ' . $e->getMessage()
+            ];
+        }
+    }
+}
+?>
--- pages/places/faculty.php
+++ pages/places/faculty.php
@@ -93,7 +93,7 @@
           }
         });
       });
-      
+
       $("#formAgregar").submit(function (e) {
         e.preventDefault();
         nombre = $("#nombreA").val();
@@ -271,7 +271,7 @@
       <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <ul class="nav">
           <li class="nav-item">
-            <a class="nav-link" href="../../index.html">
+            <a class="nav-link" href="../../index.php">
               <i class="icon-grid menu-icon"></i>
               <span class="menu-title">Dashboard</span>
             </a>
