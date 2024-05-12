cc207b0a3cdf9526d93b76d10a6e5f7883c8e45f
Cifu2002 <estebancifu88@gmail.com>
2024-05-11 20:59:27 -0500

    Se añadio actualizarFacultades al Rest

--- Acciones/Rest.php
+++ Acciones/Rest.php
@@ -12,7 +12,7 @@ switch ($accion) {
                 $json_input = file_get_contents('php://input');
                 $data = json_decode($json_input, true);
                 $nombre = filter_var($data['nombre'], FILTER_SANITIZE_STRING);
-                $descripcion =filter_var($data['descripcion'], FILTER_SANITIZE_STRING);
+                $descripcion = filter_var($data['descripcion'], FILTER_SANITIZE_STRING);
                 $campus = filter_var($data['campus'], FILTER_SANITIZE_STRING);
 
                 $resultado = AccionesFacultades::insertarFacultades($nombre, $descripcion, $campus);
@@ -23,6 +23,23 @@ switch ($accion) {
                 } else {
                     http_response_code(400);
                     echo json_encode(["message" => "No se pudo insertar la facultad."]);
+                }
+                break;
+            case 2:
+                $json_input = file_get_contents('php://input');
+                $data = json_decode($json_input, true);
+                $id = filter_var($data['id'], FILTER_SANITIZE_STRING);
+                $nombre = filter_var($data['nombre'], FILTER_SANITIZE_STRING);
+                $descripcion = filter_var($data['descripcion'], FILTER_SANITIZE_STRING);
+                $campus = filter_var($data['campus'], FILTER_SANITIZE_STRING);
+                $resultado = AccionesFacultades::actualizarFacultades($id, $nombre, $descripcion, $campus);
+
+                if ($resultado === 0) {
+                    http_response_code(200);
+                    echo json_encode(["message" => "Facultad actualizada con éxito."]);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(["message" => "No se pudo actualizar la facultad."]);
                 }
                 break;
             default:
