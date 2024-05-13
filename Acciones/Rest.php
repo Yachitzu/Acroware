8ca81c07bd8fa2e1b957c3467def57448c161b54
Cifu2002 <estebancifu88@gmail.com>
2024-05-12 21:33:50 -0500

    Se añadio insertarUbicaciones al Rest

--- Acciones/Rest.php
+++ Acciones/Rest.php
@@ -157,6 +157,23 @@ switch ($accion) {
                     echo json_encode(["message" => "No se pudo eliminar el área."]);
                 }
                 break;
+
+            //UBICACIONES
+            case 10:
+                $json_input = file_get_contents('php://input');
+                $data = json_decode($json_input, true);
+                $nombre = filter_var($data['nombre'], FILTER_SANITIZE_STRING);
+                $descripcion = filter_var($data['descripcion'], FILTER_SANITIZE_STRING);
+                $id_area_per = filter_var($data['id_area_per'], FILTER_SANITIZE_STRING);
+                $resultado = AccionesUbicaciones::insertarUbicaciones($nombre, $descripcion, $id_area_per);
+                if ($resultado === 0) {
+                    http_response_code(200);
+                    echo json_encode(["message" => "Ubicacion insertada con éxito."]);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(["message" => "No se pudo insertar la ubicación."]);
+                }
+                break;
             default:
                 break;
         }
