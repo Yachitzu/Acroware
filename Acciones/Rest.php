a1c86e40e71546ce0bb65352fcb0a6c57b3c6814
Cifu2002 <estebancifu88@gmail.com>
2024-05-12 21:35:39 -0500

    Se añadio actualizarUbicaciones al Rest

--- Acciones/Rest.php
+++ Acciones/Rest.php
@@ -174,6 +174,23 @@ switch ($accion) {
                     echo json_encode(["message" => "No se pudo insertar la ubicación."]);
                 }
                 break;
+
+            case 11:
+                $json_input = file_get_contents('php://input');
+                $data = json_decode($json_input, true);
+                $id = filter_var($data['id'], FILTER_SANITIZE_STRING);
+                $nombre = filter_var($data['nombre'], FILTER_SANITIZE_STRING);
+                $descripcion = filter_var($data['descripcion'], FILTER_SANITIZE_STRING);
+                $id_area_per = filter_var($data['id_area_per'], FILTER_SANITIZE_STRING);
+                $resultado = AccionesUbicaciones::actualizarUbicacion($id, $nombre, $descripcion, $id_area_per);
+                if ($resultado === 0) {
+                    http_response_code(200);
+                    echo json_encode(["message" => "Ubicacion actualizada con éxito."]);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(["message" => "No se pudo actualizar la ubicación."]);
+                }
+                break;
             default:
                 break;
         }
