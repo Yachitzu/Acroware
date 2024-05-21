fd1ac353037127cad3c05deaf169c2f0f38d4c0f
Nik Frías Revelo <114356147+Splinnk@users.noreply.github.com>
2024-05-21 02:20:17 -0500

    Update CrudUsuarios.php
    Se cambiaron sentencias para poder manejar de manera más dinámica las funciones

--- Acciones/CrudUsuarios.php
+++ Acciones/CrudUsuarios.php
@@ -30,40 +30,52 @@ Class Obtener{
         }
     }
     Class Guardar{
-        public static function GuardarUsuario(){
-            $conectar = Conexion::getInstance()->getConexion();
-            $nombre=$_POST["nombre"];
-            $apellido=$_POST["apellido"];
-            $cedula=$_POST["cedula"]; //los que estan en "" sonlos nombres de los objetos de el html
-            $email=$_POST["email"];
-            $password=$_POST["password"];
-            $rol=$_POST["rol"];
-            $fecha_ingreso=$_POST["fecha_ingreso"];
+        public static function GuardarUsuario()
+    {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+        $conectar = Conexion::getInstance()->getConexion();
+        if ($data !== null) {
+            $cedula = $data["cedula"];
+            $nombre = $data["nombre"];
+            $apellido = $data["apellido"];
+            $email = $data["email"];
+            $rol = $data["rol"];
+            $psswd = $data["psswd"];
 
-            $insertarSql="INSERT INTO usuarios(nombre,apellido,cedula,email,`password`, rol, fecha_ingreso)VALUES('$nombre','$apellido','$cedula','$email','$password','$rol','$fecha_ingreso')";
-            $resultado=$conectar->prepare($insertarSql);
-            $resultado->execute();
-            $conectar->commit();
         }
+
+        $insertarSql = "INSERT INTO usuarios(cedula,nombre,apellido,email,rol, psswd)VALUES('$cedula','$nombre','$apellido','$email','$rol','$psswd')";
+        $resultado = $conectar->prepare($insertarSql);
+        $resultado->execute();
+        //$conectar->commit();
+    }
     }
     class Actualizar{
-        public static function ActualizarUsuario($cedula,$nombre,$apellido,$password){
+        public static function ActualizarUsuario($id){
+            $json = file_get_contents('php://input');
+            $data = json_decode($json, true);
             $conectar = Conexion::getInstance()->getConexion();
-            $updatesql = "UPDATE usuarios  set nombre= '$nombre',apellido= '$apellido', 
-            password= '$password' Where cedula='$cedula'";
+            if ($data !== null) {
+                $email = $data["email"];
+                $rol = $data["rol"];
+            }
+
+            $updatesql = "UPDATE usuarios  set email= '$email',rol= '$rol' Where id='$id'";
             $resultado = $conectar->prepare($updatesql);
             $resultado->execute();
             echo json_encode($resultado);
-            $conectar->commit();    
+            //$conectar->commit();
         }
     }  
     class Eliminar{
-        public static function BorrarEstudiantes($cedula){
+        public static function BorrarUsuario($id)
+        {
             $conectar = Conexion::getInstance()->getConexion();
-            $borrarSQL = "DELETE FROM usuarios WHERE cedula='$cedula'";
+            $borrarSQL = "DELETE FROM usuarios WHERE id ='$id'";
             $resultado = $conectar->prepare($borrarSQL);
             $resultado->execute();
-            $conectar->commit();
+            //$conectar->commit();
             if ($resultado->rowCount() > 0) {
                 echo json_encode("Se eliminaron: " . $resultado->rowCount() . " registros");
             } else {
