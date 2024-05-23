984eb302e4216d478ed52fa466d5ad8b37f636c2
Nik Frías Revelo <114356147+Splinnk@users.noreply.github.com>
2024-05-22 22:05:05 -0500

    Creación del archivo RestSoftware
    Se ha creado el archivo RestSoftware.php que nos permite llamar a las clases del crudSoftware y se hace el llamado a la primera clase con sus dos funciones

---
+++ Acciones/RestSoftware.php
@@ -0,0 +1,12 @@
+<?php
+include_once('crudSoftware.php');
+$opc=$_SERVER["REQUEST_METHOD"];
+switch ($opc) {
+        case 'GET':
+            if (isset($_GET['id'])) {
+                Obtener::ObtenerById($_GET['id']);
+            }else{
+                Obtener::ObtenerSoftware();
+            }
+        }
+?>
