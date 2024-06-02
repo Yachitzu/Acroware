c4bf0f83cc9273e148c2f7a8f84c1b09c73a172e
Dallex MT <dallin.miranda@yahoo.es>
2024-06-02 12:42:18 -0500

    Estructura base para las Repotenciaciones

---
+++ Acciones/crudRepotenciaciones.php
@@ -0,0 +1,30 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/Acroware/patrones/Singleton/Conexion.php';
+
+class Obtener{
+    public static function ObtenerMarca(){
+        
+    }
+
+    public static function ObtenerById($id){
+        
+    }
+}
+
+class Guardar{
+    public static function GuardarMarca(){
+        
+    }
+}
+
+class Actualizar{
+    public static function ActualizarMarca($id){
+        
+    }
+}
+
+class Eliminar{
+    public static function BorrarMarca($id){
+        
+    }
+}
