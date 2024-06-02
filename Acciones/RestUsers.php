2759ee81de393a8e21bda44f8c628c384ef3a349
Naxk <silank@outlook.com>
2024-06-01 20:17:51 -0500

    se modificó el rest de usuarios para seguridad

--- Acciones/RestUsers.php
+++ Acciones/RestUsers.php
@@ -1,24 +1,38 @@
 <?php
-include_once('CrudUsuarios.php');
-$opc=$_SERVER["REQUEST_METHOD"];
+include_once ('CrudUsuarios.php');
+
+header('Content-Type: application/json');
+
+$opc = $_SERVER["REQUEST_METHOD"];
 switch ($opc) {
-        case 'GET':
-            if (isset($_GET['id'])) {
-                Obtener::ObtenerById($_GET['id']);
-            }else{
-                Obtener::ObtenerUsuarios();
-            }
-            break;
-        case 'POST':
-            Guardar::GuardarUsuario();
-            break;
-        case "DELETE":
-            $id = $_GET['id'];
-            Eliminar::BorrarUsuario($id);
-            break;
-        case "PUT":
-            $id = $_GET["id"];
-            Actualizar::ActualizarUsuario($id);
-            break;
-    }
-?>
+    case 'GET':
+        if (isset($_GET['id']) && !empty($_GET['id'])) {
+            Obtener::ObtenerById($_GET['id']);
+        } else {
+            Obtener::ObtenerUsuarios();
+        }
+        break;
+    case 'POST':
+        Guardar::GuardarUsuario();
+        break;
+    case 'DELETE':
+        $data = json_decode(file_get_contents('php://input'), true);
+        if (isset($_GET['id']) && !empty($_GET['id'])) {
+            Eliminar::BorrarUsuario($_GET['id']);
+        } else {
+            echo json_encode(['success' => false, 'message' => 'ID no proporcionado']);
+        }
+        break;
+    case 'PUT':
+        $data = json_decode(file_get_contents('php://input'), true);
+        if (isset($data['id']) && !empty($data['id'])) {
+            Actualizar::ActualizarUsuario($data['id']);
+        } else {
+            echo json_encode(['success' => false, 'message' => 'ID no proporcionado']);
+        }
+        break;
+    default:
+        echo json_encode(['success' => false, 'message' => 'Método HTTP no soportado']);
+        break;
+}
+
