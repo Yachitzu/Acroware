037936619265c5d570c5eed58eb3fa3ffc6d08f0
KidBucle64 <4ndres100@gmail.com>
2024-06-03 18:57:14 -0500

    Se crea la función para listar los bienes mobiliarios dentro del archivo que maneja el CRUD

---
+++ Acciones/crudBienes_Mobiliarios.php
@@ -0,0 +1,69 @@
+<?php
+include_once ($_SERVER['DOCUMENT_ROOT'] . '/Acroware/patrones/Singleton/Conexion.php');
+class AccionesBienes_mobiliarios
+{
+    public static function listarBienes_mobiliarios()
+    {
+        try {
+            $conexion = Conexion::getInstance()->getConexion();
+            $consulta = $conexion->prepare("SELECT bienes_mobiliarios.*, marcas.nombre AS nombre_marca, areas.nombre AS nombre_area,
+            ubicaciones.nombre AS nombre_ubicacion
+            From bienes_mobiliarios
+            LEFT JOIN marcas ON bienes_mobiliarios.id_marca = marcas.id
+            LEFT JOIN areas ON bienes_mobiliarios.id_area_per = areas.id
+            LEFT JOIN ubicaciones ON bienes_mobiliarios.id_ubi_per = ubicaciones.id
+            where bienes_mobiliarios.activo='si'");
+            $consulta->execute();
+            $dato = $consulta->fetchAll(PDO::FETCH_ASSOC);
+            $dato;
+            $tabla = '';
+
+            foreach ($dato as $respuesta) {
+                $tabla .= '
+                    <tr>
+                        <td ">' . htmlspecialchars($respuesta['codigo_uta']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['nombre']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['serie']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['nombre_marca']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['modelo']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['color']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['material']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['dimensiones']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['condicion']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['custodio_actual']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['fecha_ingreso']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['valor_contable']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['nombre_area']) . '</td>
+                        <td ">' . htmlspecialchars($respuesta['nombre_ubicacion']) . '</td>
+                        <td class="mdl-data-table__cell">
+                            <center>
+                            <button class="btn btn-warning btn-circle element-white editar" data-id="' . $respuesta['id'] . '" id="editar">
+                            <i class="fas fa-edit"></i>
+                        </button>
+                        <button class="btn btn-danger btn-circle eliminar" data-id="' . $respuesta['id'] . '" id="eliminar">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                            </center>
+                        </td>
+                        <input type="hidden" class="id_marca" value="' . htmlspecialchars($respuesta['id_marca']) . '">
+                        <input type="hidden" class="id_area_per" value="' . htmlspecialchars($respuesta['id_area_per']) . '">
+                        <input type="hidden" class="id_ubi_per" value="' . htmlspecialchars($respuesta['id_ubi_per']) . '">
+                    </tr>
+                ';
+            }
+            return [
+                'codigo' => 0,
+                'dato' => $tabla,
+            ];
+        } catch (PDOException $e) {
+            error_log('Error al listar bienes mobiliarios: ' . $e->getMessage());
+            return [
+                'codigo' => 1,
+                'mensaje' => 'Error al listar bienes mobiliarios: ' . $e->getMessage()
+            ];
+        }
+    }
+
+    
+}
+?>
