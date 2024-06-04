ac56c674f1a902241377648fddf628bb8fe691d8
KidBucle64 <4ndres100@gmail.com>
2024-06-03 19:16:06 -0500

    Se realiza el caso con POST

--- Acciones/RestBienes_Mobiliarios.php
+++ Acciones/RestBienes_Mobiliarios.php
@@ -7,7 +7,29 @@ switch ($op) {
         echo json_encode($resultado);  
         break;
     case 'POST':
-        
+        $json_input = file_get_contents('php://input');
+        $data = json_decode($json_input, true);
+        $codigo_uta = filter_var($data['codigo_uta'], FILTER_SANITIZE_STRING);
+        $nombre = filter_var($data['nombre'], FILTER_SANITIZE_STRING);
+        $serie = filter_var($data['serie'], FILTER_SANITIZE_STRING);
+        $id_marca = filter_var($data['id_marca'], FILTER_SANITIZE_STRING);
+        $modelo = filter_var($data['modelo'], FILTER_SANITIZE_STRING);
+        $color = filter_var($data['color'], FILTER_SANITIZE_STRING);
+        $material = filter_var($data['material'], FILTER_SANITIZE_STRING);
+        $dimensiones = filter_var($data['dimensiones'], FILTER_SANITIZE_STRING);
+        $condicion = filter_var($data['condicion'], FILTER_SANITIZE_STRING);
+        $custodio = filter_var($data['custodio'], FILTER_SANITIZE_STRING);
+        $valor = filter_var($data['valor'], FILTER_SANITIZE_STRING);
+        $id_area_per = filter_var($data['id_area_per'], FILTER_SANITIZE_STRING);
+        $id_ubi_per = filter_var($data['id_ubi_per'], FILTER_SANITIZE_STRING);
+        $resultado = AccionesBienes_mobiliarios::insertarBienes_mobiliarios($codigo_uta, $nombre, $serie, $id_marca, $modelo, $color, $material, $dimensiones, $condicion, $custodio, $valor, $id_area_per, $id_ubi_per);
+        if ($resultado === 0) {
+            http_response_code(200);
+            echo json_encode(["message" => "El bien mobiliario ha sido insertado con éxito."]);
+        } else {
+            http_response_code(400);
+            echo json_encode(["message" => "No se pudo insertar el bien mobiliario."]);
+        }
         break;
     case "PUT":
        
