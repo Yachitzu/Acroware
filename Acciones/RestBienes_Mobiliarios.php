63f564705ff1a9a974680e4a7f409c4806d97665
KidBucle64 <4ndres100@gmail.com>
2024-06-03 19:14:06 -0500

    Se codifica la estructura switch-case que servirá para ejecutar las funciones medinte condiciones y requests a la base de datos

---
+++ Acciones/RestBienes_Mobiliarios.php
@@ -0,0 +1,18 @@
+<?php
+include_once ("crudBienes_Mobiliarios.php");
+$op = $_SERVER["REQUEST_METHOD"];
+switch ($op) {
+    case 'GET':
+        
+        break;
+    case 'POST':
+        
+        break;
+    case "PUT":
+       
+        break;
+    case "DELETE":
+       
+        break;
+}
+?>
