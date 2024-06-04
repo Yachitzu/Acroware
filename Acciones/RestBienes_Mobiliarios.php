3d5d18cbccba121cedbe94eda73398ae92af30a5
KidBucle64 <4ndres100@gmail.com>
2024-06-03 19:15:13 -0500

    Se realiza el caso con GET

--- Acciones/RestBienes_Mobiliarios.php
+++ Acciones/RestBienes_Mobiliarios.php
@@ -3,7 +3,8 @@ include_once ("crudBienes_Mobiliarios.php");
 $op = $_SERVER["REQUEST_METHOD"];
 switch ($op) {
     case 'GET':
-        
+        $resultado = AccionesBienes_mobiliarios::listarBienes_mobiliarios();
+        echo json_encode($resultado);  
         break;
     case 'POST':
         
