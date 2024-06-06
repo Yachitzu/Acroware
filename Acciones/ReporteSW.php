56e07eb8d7451647e6e24a71300b3c056dd94684
Naxk <silank@outlook.com>
2024-06-06 01:15:09 -0500

    Se dio funcionalidad al reporte pdf

--- Acciones/ReporteSW.php
+++ Acciones/ReporteSW.php
@@ -1,112 +1,122 @@
+
 <?php
 
-require_once('fpdf/fpdf.php'); //Llamando a la Libreria TCPDF
+header('Content-Type: application/json');
 $op = $_SERVER["REQUEST_METHOD"];
-class PDF extends FPDF
-{
-
-   // Cabecera de página
-   function Header()
+if ($op == "POST" && $_POST["tipoArchivoSW"] == "pdf") {
+   
+   require_once('fpdf/fpdf.php'); //Llamando a la Libreria TCPDF
+   class PDF extends FPDF
    {
-      //include '../../recursos/Recurso_conexion_bd.php';//llamamos a la conexion BD
-
-      //$consulta_info = $conexion->query(" select *from hotel ");//traemos datos de la empresa desde BD
-      //$dato_info = $consulta_info->fetch_object();
-      $this->Image('../resources/images/logos/acroware-mini.png', 270, 5, 20); //logo de la empresa,moverDerecha,moverAbajo,tamañoIMG
-      $this->SetFont('Arial', 'B', 19); //tipo fuente, negrita(B-I-U-BIU), tamañoTexto
-      $this->Cell(95); // Movernos a la derecha
-      $this->SetTextColor(0, 0, 0); //color
-      //creamos una celda o fila
-      $this->Cell(110, 15, utf8_decode('CODECRAFTERS'), 1, 1, 'C', 0); // AnchoCelda,AltoCelda,titulo,borde(1-0),saltoLinea(1-0),posicion(L-C-R),ColorFondo(1-0)
-      $this->Ln(3); // Salto de línea
-      $this->SetTextColor(103); //color
-
-    //   /* UBICACION */
-    //   $this->Cell(180);  // mover a la derecha
-    //   $this->SetFont('Arial', 'B', 10);
-    //   $this->Cell(96, 10, utf8_decode("Ubicación : "), 0, 0, '', 0);
-    //   $this->Ln(5);
-
-    //   /* TELEFONO */
-    //   $this->Cell(180);  // mover a la derecha
-    //   $this->SetFont('Arial', 'B', 10);
-    //   $this->Cell(59, 10, utf8_decode("Teléfono : "), 0, 0, '', 0);
-    //   $this->Ln(5);
-
-    //   /* COREEO */
-    //   $this->Cell(180);  // mover a la derecha
-    //   $this->SetFont('Arial', 'B', 10);
-    //   $this->Cell(85, 10, utf8_decode("Correo : "), 0, 0, '', 0);
-    //   $this->Ln(5);
-
-    //   /* TELEFONO */
-    //   $this->Cell(180);  // mover a la derecha
-    //   $this->SetFont('Arial', 'B', 10);
-    //   $this->Cell(85, 10, utf8_decode("Sucursal : "), 0, 0, '', 0);
-    //   $this->Ln(10);
-
-      /* TITULO DE LA TABLA */
-      //color
-      $this->SetTextColor(228, 100, 0);
-      $this->Cell(100); // mover a la derecha
-      $this->SetFont('Arial', 'B', 15);
-      $this->Cell(100, 10, utf8_decode("REPORTE DE BIENES DE SOFTWARE "), 0, 1, 'C', 0);
-      $this->Ln(7);
-
-      /* CAMPOS DE LA TABLA */
-      //color
-      $this->SetFillColor(228, 100, 0); //colorFondo
-      $this->SetTextColor(255, 255, 255); //colorTexto
-      $this->SetDrawColor(163, 163, 163); //colorBorde
-      $this->SetFont('Arial', 'B', 11);
-      $this->Cell(30, 10, utf8_decode('NOMBRE'), 1, 0, 'C', 1);
-      $this->Cell(40, 10, utf8_decode('PROVEEDOR'), 1, 0, 'C', 1);
-      $this->Cell(30, 10, utf8_decode('ACTIVO'), 1, 0, 'C', 1);
-      $this->Cell(55, 10, utf8_decode('TIPO DE LICENCIA'), 1, 0, 'C', 1);
-      $this->Cell(55, 10, utf8_decode('FECHA DE ADQUISICIÓN'), 1, 0, 'C', 1);
-      $this->Cell(55, 10, utf8_decode('FECHA DE ACTIVACIÓN'), 1, 1, 'C', 1);
+      // Cabecera de página
+      function Header()
+      {
+         $this->Image('../resources/images/logos/acroware-mini.png', 270, 5, 20); //logo de la empresa,moverDerecha,moverAbajo,tamañoIMG
+         $this->SetFont('Arial', 'B', 19); //tipo fuente, negrita(B-I-U-BIU), tamañoTexto
+         $this->Cell(95); // Movernos a la derecha
+         $this->SetTextColor(0, 0, 0); //color
+         //creamos una celda o fila
+         $this->Cell(110, 15, utf8_decode('CODECRAFTERS'), 1, 1, 'C', 0); // AnchoCelda,AltoCelda,titulo,borde(1-0),saltoLinea(1-0),posicion(L-C-R),ColorFondo(1-0)
+         $this->Ln(3); // Salto de línea
+         $this->SetTextColor(103); //color
+   
+         /* TITULO DE LA TABLA */
+         //color
+         $this->SetTextColor(228, 100, 0);
+         $this->Cell(100); // mover a la derecha
+         $this->SetFont('Arial', 'B', 15);
+         $this->Cell(100, 10, utf8_decode("REPORTE DE BIENES DE SOFTWARE "), 0, 1, 'C', 0);
+         $this->Ln(7);
+   
+         /* CAMPOS DE LA TABLA */
+         //color
+         $this->SetFillColor(228, 100, 0); //colorFondo
+         $this->SetTextColor(255, 255, 255); //colorTexto
+         $this->SetDrawColor(163, 163, 163); //colorBorde
+         $this->SetFont('Arial', 'B', 11);
+         $this->Cell(30, 10, utf8_decode('NOMBRE'), 1, 0, 'C', 1);
+         $this->Cell(40, 10, utf8_decode('PROVEEDOR'), 1, 0, 'C', 1);
+         $this->Cell(30, 10, utf8_decode('ACTIVO'), 1, 0, 'C', 1);
+         $this->Cell(55, 10, utf8_decode('TIPO DE LICENCIA'), 1, 0, 'C', 1);
+         $this->Cell(55, 10, utf8_decode('FECHA DE COMPRA'), 1, 0, 'C', 1);
+         $this->Cell(55, 10, utf8_decode('FECHA DE ACTIVACIÓN'), 1, 1, 'C', 1);
+      }
+   
+      // Pie de página
+      function Footer()
+      {
+         global $dato;
+         $this->SetY(-15); // Posición: a 1,5 cm del final
+         $this->SetFont('Arial', 'I', 8); //tipo fuente, negrita(B-I-U-BIU), tamañoTexto
+         $this->Cell(0, 10, utf8_decode('Página ') . $this->PageNo() . '/{nb}', 0, 0, 'C'); //pie de pagina(numero de pagina)
+   
+         $this->SetY(-15); // Posición: a 1,5 cm del final
+         $this->SetFont('Arial', 'I', 8); //tipo fuente, cursiva, tamañoTexto
+         $hoy = date('d/m/Y');
+         $this->Cell(540, 10, utf8_decode($hoy), 0, 0, 'C'); // pie de pagina(fecha de pagina)
+      }
    }
+   include_once ($_SERVER['DOCUMENT_ROOT'] . '/Acroware/patrones/Singleton/Conexion.php');
+   
+   $pdf = new PDF();
+   $pdf->AddPage("landscape"); /* aqui entran dos para parametros (horientazion,tamaño)V->portrait H->landscape tamaño (A3.A4.A5.letter.legal) */
+   $pdf->AliasNbPages(); //muestra la pagina / y total de paginas
+   
+   $pdf->SetFont('Arial', '', 12);
+   $pdf->SetDrawColor(163, 163, 163); //colorBorde
 
-   // Pie de página
-   function Footer()
-   {
-      $this->SetY(-15); // Posición: a 1,5 cm del final
-      $this->SetFont('Arial', 'I', 8); //tipo fuente, negrita(B-I-U-BIU), tamañoTexto
-      $this->Cell(0, 10, utf8_decode('Página ') . $this->PageNo() . '/{nb}', 0, 0, 'C'); //pie de pagina(numero de pagina)
-
-      $this->SetY(-15); // Posición: a 1,5 cm del final
-      $this->SetFont('Arial', 'I', 8); //tipo fuente, cursiva, tamañoTexto
-      $hoy = date('d/m/Y');
-      $this->Cell(540, 10, utf8_decode($hoy), 0, 0, 'C'); // pie de pagina(fecha de pagina)
+   $conexion = Conexion::getInstance()->getConexion();
+   $licencia = $_POST["licenciaSW"];
+   $activo = $_POST["activoSW"];
+   $fechaIni = $_POST["fechaInicialSW"];
+   $fechaFin = $_POST["fechaFinalSW"];
+   $dato;
+   $resultado='';
+   if ($licencia == "any" && $activo == "any") {
+      $consulta = "SELECT * FROM software WHERE (fecha_adqui >= :fechaIni AND fecha_adqui <= :fechaFin)";
+      $resultado = $conexion->prepare($consulta);
+      $resultado->bindParam(':fechaIni', $fechaIni);
+      $resultado->bindParam(':fechaFin', $fechaFin);
+      $resultado->execute();
+   }elseif ($licencia == "any" && $activo != "any") {
+      $consulta = "SELECT * FROM software WHERE activado = :activo AND (fecha_adqui >= :fechaIni AND fecha_adqui <= :fechaFin)";
+      $resultado = $conexion->prepare($consulta);
+      $resultado->bindParam(':activo', $activo);
+      $resultado->bindParam(':fechaIni', $fechaIni);
+      $resultado->bindParam(':fechaFin', $fechaFin);
+      $resultado->execute();
+   }elseif ($licencia != "any" && $activo == "any") {
+      $consulta = "SELECT * FROM software WHERE tipo_licencia = :licencia AND (fecha_adqui >= :fechaIni AND fecha_adqui <= :fechaFin)";
+      $resultado = $conexion->prepare($consulta);
+      $resultado->bindParam(':licencia', $licencia);
+      $resultado->bindParam(':fechaIni', $fechaIni);
+      $resultado->bindParam(':fechaFin', $fechaFin);
+      $resultado->execute();
+   }else{
+      $consulta = "SELECT * FROM software WHERE tipo_licencia = :licencia AND activado = :activo AND (fecha_adqui >= :fechaIni AND fecha_adqui <= :fechaFin)";
+      $resultado = $conexion->prepare($consulta);
+      $resultado->bindParam(':licencia', $licencia);
+      $resultado->bindParam(':activo', $activo);
+      $resultado->bindParam(':fechaIni', $fechaIni);
+      $resultado->bindParam(':fechaFin', $fechaFin);
+      $resultado->execute();
    }
+   $dato = $resultado->fetchAll(PDO::FETCH_ASSOC);
+   foreach ($dato as $respuesta) {
+      /* TABLA */
+      $pdf->Cell(30, 10, utf8_decode($respuesta['nombre_software']), 1, 0, 'C', 0);
+      $pdf->Cell(40, 10, utf8_decode($respuesta['proveedor']), 1, 0, 'C', 0);
+      $pdf->Cell(30, 10, utf8_decode($respuesta['activado']), 1, 0, 'C', 0);
+      $pdf->Cell(55, 10, utf8_decode($respuesta['tipo_licencia']), 1, 0, 'C', 0);
+      $pdf->Cell(55, 10, utf8_decode($respuesta['fecha_adqui']), 1, 0, 'C', 0);
+      $pdf->Cell(55, 10, utf8_decode($respuesta['fecha_activacion']), 1, 1, 'C', 0);
+   }
+   
+   
+   
+   $pdf->Output('ReporteSW.pdf', 'I');//nombreDescarga, Visor(I->visualizar - D->descargar)
+}else if ($op == "POST" && $_POST["tipoArchivoSW"] == "excel") {
+   
+}else{
+   echo "aquí no encuentra nada si no envía info";
 }
-
-//include '../../recursos/Recurso_conexion_bd.php';
-//require '../../funciones/CortarCadena.php';
-/* CONSULTA INFORMACION DEL HOSPEDAJE */
-//$consulta_info = $conexion->query(" select *from hotel ");
-//$dato_info = $consulta_info->fetch_object();
-
-$pdf = new PDF();
-$pdf->AddPage("landscape"); /* aqui entran dos para parametros (horientazion,tamaño)V->portrait H->landscape tamaño (A3.A4.A5.letter.legal) */
-$pdf->AliasNbPages(); //muestra la pagina / y total de paginas
-
-$i = 0;
-$pdf->SetFont('Arial', '', 12);
-$pdf->SetDrawColor(163, 163, 163); //colorBorde
-
-/*$consulta_reporte_alquiler = $conexion->query("  ");*/
-
-/*while ($datos_reporte = $consulta_reporte_alquiler->fetch_object()) {      
-   }*/
-$i = $i + 1;
-/* TABLA */
-$pdf->Cell(30, 10, utf8_decode("N°"), 1, 0, 'C', 0);
-$pdf->Cell(40, 10, utf8_decode("numero"), 1, 0, 'C', 0);
-$pdf->Cell(40, 10, utf8_decode("nombre"), 1, 0, 'C', 0);
-$pdf->Cell(40, 10, utf8_decode("precio"), 1, 0, 'C', 0);
-$pdf->Cell(85, 10, utf8_decode("info"), 1, 0, 'C', 0);
-$pdf->Cell(40, 10, utf8_decode("total"), 1, 1, 'C', 0);
-
-
-$pdf->Output('ReporteSW.pdf', 'I');//nombreDescarga, Visor(I->visualizar - D->descargar)
