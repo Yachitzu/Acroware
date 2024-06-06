da5df46aabd827cc2438ab2ebef92be34dbfdedd
Naxk <silank@outlook.com>
2024-06-06 02:26:00 -0500

    se ha agregado la función del reporte en excel

--- Acciones/ReporteBI.php
+++ Acciones/ReporteBI.php
@@ -160,8 +160,123 @@ foreach ($dato as $respuesta) {
    
    
    $pdf->Output('ReporteSW.pdf', 'I');//nombreDescarga, Visor(I->visualizar - D->descargar)
-}else if ($op == "POST" && $_POST["tipoArchivoSW"] == "excel") {
-   
+}else if ($op == "POST" && $_POST["tipoArchivoI"] == "excel") {
+    header('Content-Type:text/csv; charset=UTF-8');
+    header('Content-Disposition: attachment; filename="ReporteBI.csv"');
+ 
+    $salida = fopen('php://output', 'w');
+    fputcsv($salida, array('Codigo Uta', 'Nombre', 'Serie', 'Marca', 'Modelo', 'Area', 'Ubicacion', 'ip', 'Fecha de ingreso', 'Precio', 'Activo'));
+    include_once ($_SERVER['DOCUMENT_ROOT'] . '/Acroware/patrones/Singleton/Conexion.php');
+ 
+    $conexion = Conexion::getInstance()->getConexion();
+    $area = $_POST["areaI"];
+    $marca = $_POST["marcaI"];
+    $fechaIni = $_POST["fechaInicialI"];
+    $fechaFin = $_POST["fechaFinalI"];
+    $dato;
+    $resultado='';
+
+    if ($area == "any" && $marca == "any") {
+        $consulta = "SELECT 
+                        bi.*,
+                        a.nombre as nombre_area,
+                        u.nombre as nombre_ubicacion,
+                        m.nombre as nombre_marca
+                    FROM 
+                        bienes_informaticos bi
+                    INNER JOIN 
+                        areas a ON bi.id_area_per = a.id
+                    INNER JOIN 
+                        ubicaciones u ON bi.id_ubi_per = u.id
+                    INNER JOIN 
+                        marcas m ON bi.id_marca = m.id
+                    WHERE 
+                        (bi.fecha_ingreso >= :fechaIni AND bi.fecha_ingreso <= :fechaFin)";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->bindParam(':fechaIni', $fechaIni);
+        $resultado->bindParam(':fechaFin', $fechaFin);
+        $resultado->execute();
+    }elseif($area == "any" && $marca != "any") {
+        $consulta = "SELECT 
+                        bi.*,
+                        a.nombre as nombre_area,
+                        u.nombre as nombre_ubicacion,
+                        m.nombre as nombre_marca
+                    FROM 
+                        bienes_informaticos bi
+                    INNER JOIN 
+                        areas a ON bi.id_area_per = a.id
+                    INNER JOIN 
+                        ubicaciones u ON bi.id_ubi_per = u.id
+                    INNER JOIN 
+                        marcas m ON bi.id_marca = m.id
+                    WHERE 
+                        id_marca = :marca AND (bi.fecha_ingreso >= :fechaIni AND bi.fecha_ingreso <= :fechaFin)";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->bindParam(':marca', $marca);
+        $resultado->bindParam(':fechaIni', $fechaIni);
+        $resultado->bindParam(':fechaFin', $fechaFin);
+        $resultado->execute();
+    }elseif($area != "any" && $marca == "any") {
+        $consulta = "SELECT 
+                        bi.*,
+                        a.nombre as nombre_area,
+                        u.nombre as nombre_ubicacion,
+                        m.nombre as nombre_marca
+                    FROM 
+                        bienes_informaticos bi
+                    INNER JOIN 
+                        areas a ON bi.id_area_per = a.id
+                    INNER JOIN 
+                        ubicaciones u ON bi.id_ubi_per = u.id
+                    INNER JOIN 
+                        marcas m ON bi.id_marca = m.id
+                    WHERE 
+                        bi.id_area_per = :area AND (bi.fecha_ingreso >= :fechaIni AND bi.fecha_ingreso <= :fechaFin)";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->bindParam(':area', $area);
+        $resultado->bindParam(':fechaIni', $fechaIni);
+        $resultado->bindParam(':fechaFin', $fechaFin);
+        $resultado->execute();
+    }else {
+        $consulta = "SELECT 
+                        bi.*,
+                        a.nombre as nombre_area,
+                        u.nombre as nombre_ubicacion,
+                        m.nombre as nombre_marca
+                    FROM 
+                        bienes_informaticos bi
+                    INNER JOIN 
+                        areas a ON bi.id_area_per = a.id
+                    INNER JOIN 
+                        ubicaciones u ON bi.id_ubi_per = u.id
+                    INNER JOIN 
+                        marcas m ON bi.id_marca = m.id
+                    WHERE 
+                        id_marca = :marca AND bi.id_area_per = :area AND (bi.fecha_ingreso >= :fechaIni AND bi.fecha_ingreso <= :fechaFin)";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->bindParam(':marca', $marca);
+        $resultado->bindParam(':area', $area);
+        $resultado->bindParam(':fechaIni', $fechaIni);
+        $resultado->bindParam(':fechaFin', $fechaFin);
+        $resultado->execute();
+    }
+
+    $dato = $resultado->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($dato as $respuesta) {
+        fputcsv($salida, array(utf8_encode($respuesta['codigo_uta']),
+        utf8_encode($respuesta['nombre']),
+        utf8_encode($respuesta['serie']),
+        utf8_encode($respuesta['nombre_marca']),
+        utf8_encode($respuesta['modelo']),
+        utf8_encode($respuesta['nombre_area']),
+        utf8_encode($respuesta['nombre_ubicacion']),
+        utf8_encode($respuesta['ip']),
+        utf8_encode($respuesta['fecha_ingreso']),
+        utf8_encode($respuesta['precio']),
+        utf8_encode($respuesta['activo'])));
+    }
 }else{
    echo "aquí no encuentra nada si no envía info";
 }
