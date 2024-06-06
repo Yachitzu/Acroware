d8297e635c54e12408c50928d25ff094e71d741f
Naxk <silank@outlook.com>
2024-06-06 01:16:56 -0500

    Se dio funcionalidad al reporte excel

--- Acciones/ReporteSW.php
+++ Acciones/ReporteSW.php
@@ -106,17 +106,70 @@ if ($op == "POST" && $_POST["tipoArchivoSW"] == "pdf") {
       /* TABLA */
       $pdf->Cell(30, 10, utf8_decode($respuesta['nombre_software']), 1, 0, 'C', 0);
       $pdf->Cell(40, 10, utf8_decode($respuesta['proveedor']), 1, 0, 'C', 0);
-      $pdf->Cell(30, 10, utf8_decode($respuesta['activado']), 1, 0, 'C', 0);
-      $pdf->Cell(55, 10, utf8_decode($respuesta['tipo_licencia']), 1, 0, 'C', 0);
-      $pdf->Cell(55, 10, utf8_decode($respuesta['fecha_adqui']), 1, 0, 'C', 0);
-      $pdf->Cell(55, 10, utf8_decode($respuesta['fecha_activacion']), 1, 1, 'C', 0);
+      $pdf->Cell(40, 10, utf8_decode($respuesta['activado']), 1, 0, 'C', 0);
+      $pdf->Cell(40, 10, utf8_decode($respuesta['tipo_licencia']), 1, 0, 'C', 0);
+      $pdf->Cell(85, 10, utf8_decode($respuesta['fecha_adqui']), 1, 0, 'C', 0);
+      $pdf->Cell(40, 10, utf8_decode($respuesta['fecha_activacion']), 1, 1, 'C', 0);
    }
    
    
    
    $pdf->Output('ReporteSW.pdf', 'I');//nombreDescarga, Visor(I->visualizar - D->descargar)
 }else if ($op == "POST" && $_POST["tipoArchivoSW"] == "excel") {
-   
+   header('Content-Type:text/csv; charset=UTF-8');
+   header('Content-Disposition: attachment; filename="ReporteSW.csv"');
+
+   $salida = fopen('php://output', 'w');
+   fputcsv($salida, array('Nombre', 'Proveedor', 'Activo', 'Tipo de licencia', 'Fecha de compra', 'Fecha de Activación'));
+   include_once ($_SERVER['DOCUMENT_ROOT'] . '/Acroware/patrones/Singleton/Conexion.php');
+
+   $conexion = Conexion::getInstance()->getConexion();
+   $conexion->exec("SET NAMES 'utf8'");
+   $licencia = $_POST["licenciaSW"];
+   $activo = $_POST["activoSW"];
+   $fechaIni = $_POST["fechaInicialSW"];
+   $fechaFin = $_POST["fechaFinalSW"];
+   $dato;
+   $resultado='';
+   if ($licencia == "any" && $activo == "any") {
+      $consulta = "SELECT * FROM software WHERE (fecha_adqui >= :fechaIni AND fecha_adqui <= :fechaFin)";
+      $resultado = $conexion->prepare($consulta);
+      $resultado->bindParam(':fechaIni', $fechaIni);
+      $resultado->bindParam(':fechaFin', $fechaFin);
+      $resultado->execute();
+   }elseif ($licencia == "any" && $activo != "any") {
+      $consulta = "SELECT * FROM software WHERE activado = :activo AND (fecha_adqui >= :fechaIni AND fecha_adqui <= :fechaFin)";
+      $resultado = $conexion->prepare($consulta);
+      $resultado->bindParam(':activo', $activo);
+      $resultado->bindParam(':fechaIni', $fechaIni);
+      $resultado->bindParam(':fechaFin', $fechaFin);
+      $resultado->execute();
+   }elseif ($licencia != "any" && $activo == "any") {
+      $consulta = "SELECT * FROM software WHERE tipo_licencia = :licencia AND (fecha_adqui >= :fechaIni AND fecha_adqui <= :fechaFin)";
+      $resultado = $conexion->prepare($consulta);
+      $resultado->bindParam(':licencia', $licencia);
+      $resultado->bindParam(':fechaIni', $fechaIni);
+      $resultado->bindParam(':fechaFin', $fechaFin);
+      $resultado->execute();
+   }else{
+      $consulta = "SELECT * FROM software WHERE tipo_licencia = :licencia AND activado = :activo AND (fecha_adqui >= :fechaIni AND fecha_adqui <= :fechaFin)";
+      $resultado = $conexion->prepare($consulta);
+      $resultado->bindParam(':licencia', $licencia);
+      $resultado->bindParam(':activo', $activo);
+      $resultado->bindParam(':fechaIni', $fechaIni);
+      $resultado->bindParam(':fechaFin', $fechaFin);
+      $resultado->execute();
+   }
+   $dato = $resultado->fetchAll(PDO::FETCH_ASSOC);
+
+   foreach ($dato as $respuesta) {
+      fputcsv($salida, array(utf8_encode($respuesta['nombre_software']),
+      utf8_encode($respuesta['proveedor']),
+      utf8_encode($respuesta['activado']),
+      utf8_encode($respuesta['tipo_licencia']),
+      utf8_encode($respuesta['fecha_adqui']),
+      utf8_encode($respuesta['fecha_activacion'])));
+   }
 }else{
    echo "aquí no encuentra nada si no envía info";
 }
